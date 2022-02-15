087bc7325730f2474f7b25fc69985d46da179c3f
Ihor <nikonovir@gmail.com>
2022-02-15 20:01:44 +0200

    js

---
+++ assests/js/Lesson_22.js
@@ -0,0 +1,96 @@
+// let
+// var
+// const
+// let message=1234
+// console.log(message)
+
+// Number
+//let n=1234
+//console.log(n)
+//console.log(1/0)
+//console.log(-1/0)
+//console.log("two"/3)
+
+// DigInt
+
+//let bigNum=2324423424424442444242
+//console.log(bigNum)
+
+// String
+//let str1='Одиничні'
+//let str2="Подвійні"
+//let str3=`Обернені`
+//let name="Владислав"
+//let age = 19
+//console.log(`Мене звати ${name}, мені ${age} років`)
+//console.log("Мене звати" + name + ", мені " + age + "років")
+
+//console.log(`Сума двох чисел 2 і 5 = ${2+5}`)
+
+// Booolean
+//let nameFieldChecked = true
+//console.log(nameFieldChecked)
+//let isGreat = 4<1
+//console.log(isGreat)
+
+//null
+//let age =null
+//console.log(age)
+
+//undefined
+//let a 
+//console.log(a)
+
+// Object
+// Symbol
+
+// typeof
+
+//console.log(typeof true)
+//let a=18
+//console.log( a)
+
+// let value=true
+// console.log(typeof value)
+// //value=String(value)
+// //console.log(typeof value)
+// value=Number(value)
+// console.log(typeof value)
+// console.log(value)
+
+// let str ="113"
+// str=Number(str)
+// console.log(str)
+// console.log(typeof str)
+
+// let numb=true
+// numb=Number(numb)
+// console.log(numb)
+
+// let str ="text"
+// str=Boolean(str)
+// console.log(str)
+
+//Basic Opertors
+// операнд
+// бінарний 
+// унарний
+
+// let a = 1
+// let b=4
+// a= -a
+// console.log(a+b)
+
+
+// +
+// -
+// *
+// /
+// %
+// **
+// Math
+// %
+// console.log(18%5)
+
+// **
+console.log(2**4)
---
+++ assests/js/Lesson_23.js
@@ -0,0 +1,120 @@
+// task_1
+
+// let num=3
+// console.log(num)
+
+// task_2
+
+// let a=10
+// let b=2
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+
+// task_3
+
+// let c=15
+// let d=2
+// let result=c+d
+// console.log(result)
+
+// task_4
+
+// let a=10
+// let b=2
+// let c=5
+// let sum=a+b+c
+// console.log(sum)
+
+// task_5
+
+// let a=17
+// let b=10
+// let d=7
+// let c=a-b
+// let result=c+d
+// console.log(c)
+// console.log(result)
+
+// task_6
+
+// let str=`Привіт світ`
+// console.log(str)
+
+// task_7
+
+// let str1=`Привіт`
+// let str2=`Світ!`
+// console.log(`${str1}, ${str2}`)
+
+// task_8
+
+// let name=`Igoooooooor!`
+// console.log(`Привіт, ${name}`)
+
+// task_9
+
+// let age=15
+// console.log(`Мені ${age} років`)
+
+// task_10
+
+// let  name = prompt(`Як вас звати`)
+// alert(`Ваше імʼя ${name}`)
+
+// task_11
+
+// let number = prompt(`Введіть число`)
+// alert(`Ваше число ${number**2}`)
+
+// task_12
+
+// let str=`abcde`
+// console.log(str[0])
+// console.log(str[2])
+// console.log(str[4])
+
+// task_13
+
+// let num=`12345`
+// console.log(num*1)
+// console.log(num*2)
+// console.log(num*3)
+// console.log(num*4)
+// console.log(num*5)
+
+// task_14
+
+// console.log(60*60)
+// console.log(60*60*24)
+// console.log(60*60*24*30)
+
+// task_15
+
+let hour=15
+let minute=30
+let second=28
+console.log(`${hour}:${minute}:${second}`)
+
+// task_16
+
+// let num=18
+// console.log(num**2)
+
+// task_17
+
+// let num = 47
+// num += 7
+// num -= 18
+// num *= 10
+// num /= 15
+// console.log(num)
+
+// task_18
+
+// var num = 10
+// num = num ++ 
+// num = num ++ 
+// num = num -- 
+// console.log(num)
