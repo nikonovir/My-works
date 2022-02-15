fc59cf1ba0708c4115f8553a02eb579b3b692dde
Ihor <nikonovir@gmail.com>
2022-02-15 20:12:43 +0200

    final

---
+++ assests/js/Lesson_24.js
@@ -0,0 +1,23 @@
+// let year=prompt("Який зараз рік", 2022)
+// if (year==2022){
+//     console.log("good job")
+// }
+// else{
+//     console.log("bad")
+// }
+
+// let 
+// const
+// var
+
+// let year = prompt("рік публікування твіту")
+// if (year<2000){
+//     alert("Дуже рано")
+// }
+// else if (year>2015){
+//     alert("Пізно")
+// }
+// else{
+//     alert("Влучив в яблучкр")
+// }
+
